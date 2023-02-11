a5ad967a56845fb6323dc7d77b446bc133737c11
bhendi-boi <bhendiisgreen@gmail.com>
2023-02-11 11:18:38 +0530

    nav over

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,10 +1,13 @@
 import { RxHamburgerMenu, RxAvatar } from "react-icons/rx";
-import LOGO from "../public/Devfolio_Logo.png";
+import LOGO from "../public/Devfolio - Normal.png";
 const Nav = () => {
   return (
-    <nav className="bg-black h-20 flex text-white items-center">
-      <RxHamburgerMenu size={30} className="" />
-      <img src={LOGO} alt="" className="mx-auto" />
+    <nav className="h-[76px] flex text-black items-center p-8 border">
+      <div className="mr-auto flex gap-4 text-nav-inactive font-semibold">
+        <img src={LOGO} alt="" className="items-center h-[20px]" />
+        <span className="hover:text-black">Home</span>
+        <span className="hover:text-black">Blog</span>
+      </div>
       <RxAvatar size={30} />
     </nav>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "nav-inactive": "#8E989C",
+      },
+      fontFamily: {
+        body: ["Nunito Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
